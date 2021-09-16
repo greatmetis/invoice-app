ff51000a5c985665bcb0c13dd003060cb91c7f3f
Metis Teng <greatmetis@gmail.com>
2021-09-17 00:47:32 +1000

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+# build
 npm run build
 
+# navigate into the build output directory
 cd dist
 
-git init 
-git -A
-git commit -m 'New Deloyment'
-git push -f https://github.com/greatmetis/invoice-app.git main gh-pages
+git init
+git add -A
+git commit -m 'New deployment'
+git push -f https://github.com/greatmetis/invoice-app.git main:gh-pages
 
 cd -
